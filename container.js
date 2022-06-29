e8f83fc0415fae413038d49c2761a1d86976c513
dbtop <berdaddario@gmail.com>
2022-06-29 11:19:19 -0300

    server express update

---
+++ container.js
@@ -0,0 +1,39 @@
+ class Container {
+
+    constructor(name) {
+        this.name = name;
+        this.items = [];
+    }
+
+    save(item) {
+        if(!item.id){
+            item.id = idGenerator();
+        }
+        this.items = [...this.items, item];
+        return item.id;
+    }
+
+    getById(id) {
+        return this.items.find(item => item.id === id );
+    }
+    
+    getAll() {
+        return this.items;
+    }
+
+    deleteById(id) {
+        this.items = this.items.filter(item => item.id !== id)
+        return this.items;
+    }
+
+    deleteAll() {
+        this.items = [];
+    }
+}
+
+//Helper function
+function idGenerator(){
+    return Date.now().toString(32) + Math.random().toString(32).substring(2);
+}
+
+module.exports = Container;
--- index.js
+++ index.js
@@ -1,38 +1,48 @@
-// 1-import express
-// 2-create app = express()
-///3 npm i nodemon
+const fs = require('fs');
+
+const Container = require('./container.js')
+const container = new Container('products')
+
 const express = require('express')
-
-
 
 const app = express()
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8080;
 
-const server = app.listen(PORT, ()=>{
+const server = app.listen( PORT, ()=>{
     console.log(`Server Listening on port ${server.address().port}`)
-    console.log(`àpp Listening on port ${PORT}`)
 });
-
 
 server.on('error', (err)=>{
     console.log(`Cb: Error on server ${err}`)
 });
 
 
-app.get('/', (req,res)=>{
-    res.send(`<h1 style="color:blue">Bienvenidos al Server de express</h1>`)
+const fileReader = async () => {
+    const data = await fs.promises.readFile('products.txt', 'utf8')
+    const products = JSON.parse(data)
+
+    useSaveMethod(products)
+    // useGetByIdMethod(products)
+    getRandom(products)
+}
+fileReader();
+
+const useSaveMethod = (products) => {
+    products.forEach(product => {
+        container.save(product)
+    })
+    dataGet(container.getAll())
+}
+
+const dataGet = data => app.get("/products", (req, res) => {
+    res.send(data)
 });
 
-let visitas = 0;
-app.get('/visitas', (req,res)=>{
-    if (res){
-        res.send({numberOfVisits : ++visitas})
-    }
+const getRandom = products => app.get('/randomProduct', (req,res) => {
+    const id = Math.floor(Math.random() * products.length+1)
+    res.json(container.getById(id))
 });
 
 
-// new Date().toLocalString()
-app.get('/fyh', (req,res)=>{
-    res.send({FechayHora: new Date().toLocalString()})
-});
+
